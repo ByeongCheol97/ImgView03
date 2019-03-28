b23842983bb2adcad1dc07f3fdf860728894562a
D7703_13 <d7703_13@d7703-13ui-imac.local>
2019-03-28 12:50:20 +0900

    commit

--- ImgView03/ViewController.swift
+++ ImgView03/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgview: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    var count = 0
+    var isAnimating = false
+    //Timer 객체생성
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imgview.image = UIImage(named: "Image\(count)")
+        label.text = String(count)
     }
 
-
+    @IBAction func playbtn(_ sender: Any) {
+        print("playbtn")
+        //Timer 작동
+        if isAnimating == false {
+            myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+            isAnimating = true
+        }else {
+            myTimer.invalidate()
+            isAnimating = false
+        }
+    }
+    @IBAction func pausebtn(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    @IBAction func stopbtn(_ sender: Any) {
+        myTimer.invalidate()
+        count = 0
+    }
+    
+    @objc func doAnimation() {
+        if count == 5 {
+            count = 0
+        }
+        count += 1
+        
+        imgview.image = UIImage(named: "Image\(count)")
+        label.text = String(count)
+    }
 }
 
